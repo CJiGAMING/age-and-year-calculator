c8e70bf6bdbe416ccc8c1e847b8e6c9f641cee6b
CJiGAMING <91716900+CJiGAMING@users.noreply.github.com>
2022-11-08 17:05:35 +0530

    Add files via upload

---
+++ age and year claculator.py
@@ -0,0 +1,17 @@
+while True:
+    print("welcome to my program \n")
+    choice = input("""press (a) to claculate your age \n 
+      press (b) to know your birth year \n
+
+    """)
+
+    if choice == "a":
+      year_born = input("enter in which year you were born : ")
+      year_born = int(year_born)
+      print(2022 - year_born, "\n")
+
+
+    elif choice == "b":
+      age = input("enter your current age : ")
+      age = int(age)
+      print(2022 - age, "\n")
